b61a62aff8f84cbb371a4084e79207310d13df9e
aslambpract <42261475+aslambpract@users.noreply.github.com>
2021-11-27 14:09:57 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,6 +24,7 @@ class UserController extends Controller
     {
         $user = NULL; // TASK: find user by $userId or show "404 not found" page
 
+        abort_if(!User::find($userId) , 404) ;
         return view('users.show', compact('user'));
     }
 
